c7f51e966d50adb8d41a06b60170f80c583175eb
EduardoZavati <edudantesouza@gmail.com>
2025-03-17 01:00:20 -0300

    ATT APP.PY, ATT BD.PY
    Co-authored-by: André Luiz Anastácio Neves <DzyNvs@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,29 +1,125 @@
 from flask import Flask, jsonify, make_response, request 
-from bd import Alunos
+from bd import Alunos, Professores, Turmas
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
+#ALUNOS
 @app.route('/Alunos', methods=['POST'])
 def create_Alunos():
     alunos = request.json
     Alunos.append(alunos)   
-    return make_response (
-        jsonify(Alunos)
-    )
-    
-    
+    return make_response (jsonify(Alunos))
+
+
+ 
 
 @app.route('/Alunos', methods=['GET'])
 def get_Alunos():
-    return make_response (
-        jsonify(Alunos)
-    )
+    return make_response (jsonify(Alunos))
 
 
-@app.route('/Alunos', methods=['DELETE'])
-def delete_Alunos():
-    
+@app.route('/Alunos/<int:id>', methods=['GET'])
+def get_Aluno(id):
+    aluno = next((a for a in Alunos if a['id']==id), None)
+    if aluno: 
+        return make_response(jsonify({'id': aluno['id'], 'nome': aluno['nome']})),200
+    return make_response(jsonify({"erro": "Aluno não encontrado" })), 404
+
+@app.route('/Alunos/<int:id>', methods=['PUT'])
+def editar_Aluno(id):
+    aluno = next((a for a in Alunos if a['id'] == id), None)
+
+    if not aluno:
+        return make_response(jsonify({"erro": "Aluno não encontrado"}), 404)
+
+    dados = request.json
+
+    aluno['nome'] = dados.get('nome', aluno['nome'])
+    aluno['data_nascimento'] = dados.get('data_nascimento', aluno['data_nascimento'])
+    aluno['nota_primeiro_semestre'] = dados.get('nota_primeiro_semestre', aluno['nota_primeiro_semestre'])
+    aluno['nota_segundo_semestre'] = dados.get('nota_segundo_semestre', aluno['nota_segundo_semestre'])
+    aluno['turma_id'] = dados.get('turma_id', aluno['turma_id'])
+
+    return make_response(jsonify({"mensagem": f"Aluno com ID {id} atualizado com sucesso"}), 200)
+
+
+
+@app.route('/Alunos/<int:id>', methods=['PUT'])
+def editar_Alunos(id):
+    aluno = next((a for a in Alunos if a['id'] == id), None)
+
+    if not aluno:
+        return make_response(jsonify({"message": "Aluno não encontrado"}), 404)
+
+    nome_novo = request.form.get('nome')
+
+    if not nome_novo:
+        return make_response(jsonify({"erro": "Campo 'nome' é obrigatório"}), 400)
+
+    aluno["nome"] = nome_novo
+
+    return make_response(jsonify({"mensagem": f"Aluno com ID {id} atualizado para {aluno['nome']}"}), 200)
+
+
+
+#Professores 
+
+@app.route('/Professores', methods=['POST'])
+def create_Professores():
+    professores = request.json
+    Professores.append(professores)   
+    return make_response (jsonify(Professores))
+
+@app.route('/Professores', methods=['GET'])
+def get_Professores():
+    return make_response (jsonify(Professores))
+
+@app.route('/Professores/<int:id>', methods=['GET'])
+def get_Professor(id):
+    professor = next((p for p in Professores if p['id']==id), None)
+    if professor: 
+        return make_response(jsonify({'id': professor['id'], 'nome': professor['nome']})),200
+    return make_response(jsonify({"erro": "Professor não encontrado" })), 404
+
+@app.route('/Professores/<int:id>', methods=['DELETE'])
+def deletar_professor_por_id(id):
+    global Professores
+    professor = next((p for p in Professores if p ['id']==id), None)
+    if professor: 
+        Professores.remove(professor)
+        return make_response(jsonify({"mensagem": f"Professor com ID {id} deletado"})),200
+    return make_response(jsonify({"erro": 'Professor não encontrado'})), 400
+
+#Turmas
+
+@app.route('/Turmas', methods=['POST'])
+def create_Turmas():
+    turmas = request.json
+    Turmas.append(turmas)   
+    return make_response (jsonify(Turmas))
+
+@app.route('/Turmas', methods=['GET'])
+def get_Turmas():
+    return make_response (jsonify(Turmas))
+
+@app.route('/Turmas/<int:id>', methods=['GET'])
+def get_Turma(id):
+    turma = next((t for t in Professores if t['id']==id), None)
+    if turma: 
+        return make_response(jsonify({'id': turma['id'], 'nome': turma['nome']})),200
+    return make_response(jsonify({"erro": "Turma não encontrada" })), 404
+
+@app.route('/Turmas/<int:id>', methods=['DELETE'])
+def deletar_turma_por_id(id):
+    global Turmas
+    turma = next((t for t in Turmas if t ['id']==id), None)
+    if turma: 
+        Turmas.remove(turma)
+        return make_response(jsonify({"mensagem": f"Turma com ID {id} deletado"})),200
+    return make_response(jsonify({"erro": 'Turma não encontrada'})), 400
+
+
 
 if __name__  == '__main__': 
     app.run(debug=True)
--- bd.py
+++ bd.py
@@ -1,58 +1,158 @@
 Alunos = [
     {
-        'id': 1,
-        'nome': 'Eduardo Zavati',
-        'data_nascimento': "2006-05-15",
-        'nota_primeiro_semestre': 6.5,
-        'nota_segundo_semestre': 7.5,
-        'turma_id': 1
+     'id': 1,
+     'nome': 'Eduardo Zavati',
+     'data_nascimento': "2006-05-15",
+     'nota_primeiro_semestre': 6.5,
+     'nota_segundo_semestre': 7.5,
+     'turma_id': 1
     },
-    {
-        'id': 2,
-        'nome': 'João Silva',
-        'data_nascimento': "2004-03-19",
-        'nota_primeiro_semestre': 6.5,
-        'nota_segundo_semestre': 7.5,
-        'turma_id': 2
-    },
-    {
-        'id': 3,
-        'nome': 'Maria Oliveira',
-        'data_nascimento': "2002-04-11",
-        'nota_primeiro_semestre': 6.5,
-        'nota_segundo_semestre': 7.5,
-        'turma_id': 2
-    },
-    {
-        'id': 4,
-        'nome': 'Leonardo Pereira',
-        'data_nascimento': "2003-08-21",
-        'nota_primeiro_semestre': 6.5,
-        'nota_segundo_semestre': 7.5,
-        'turma_id': 2
-    },
-    {
-        'id': 5,
-        'nome': 'Cleber Machado',
-        'data_nascimento': "1994-07-09",
-        'nota_primeiro_semestre': 6.0,
-        'nota_segundo_semestre': 7.5,
-        'turma_id': 2
-    },
-    {
-        'id': 6,
-        'nome': 'Pedro Borges',
-        'data_nascimento': "1998-12-21",
-        'nota_primeiro_semestre': 6.5,
-        'nota_segundo_semestre': 6.0,
-        'turma_id': 2
-    },
-    {
-        'id': 7,
-        'nome': 'Lucas Moreira',
-        'data_nascimento': "2004-03-19",
-        'nota_primeiro_semestre': 6.5,
-        'nota_segundo_semestre': 7.5,
-        'turma_id': 2
-    }
+    {'id': 2,
+     'nome': 'João Silva',
+     'data_nascimento': "2004-03-19",
+     'nota_primeiro_semestre': 6.5,
+     'nota_segundo_semestre': 7.5,
+     'turma_id': 2
+     },
+    {'id': 3,
+     'nome': 'Maria Oliveira',
+     'data_nascimento': "2002-04-11",
+     'nota_primeiro_semestre': 6.5,
+     'nota_segundo_semestre': 7.5,
+     'turma_id': 3
+     },
+    {'id': 4,
+     'nome': 'Leonardo Pereira',
+     'data_nascimento': "2003-08-21",
+     'nota_primeiro_semestre': 6.5, 
+     'nota_segundo_semestre': 7.5,
+     'turma_id': 4
+     },
+    {'id': 5,
+     'nome': 'Cleber Machado',
+     'data_nascimento': "1994-07-09",
+     'nota_primeiro_semestre': 6.0,
+     'nota_segundo_semestre': 7.5,
+     'turma_id': 5
+     },
+    {'id': 6,
+     'nome': 'Pedro Borges',
+     'data_nascimento': "1998-12-21",
+     'nota_primeiro_semestre': 6.5,
+     'nota_segundo_semestre': 6.0,
+     'turma_id': 6
+     },
+    {'id': 7,
+     'nome': 'Lucas Moreira',
+     'data_nascimento': "2004-03-19",
+     'nota_primeiro_semestre': 6.5,
+     'nota_segundo_semestre': 7.5,
+     'turma_id': 6
+     }
 ]
+
+Professores = [
+  {
+    "id": 1,
+    "nome": "Ana Silva",
+    "idade": 30,
+    "data_nascimento": "1993-05-15",
+    "disciplina": "Matemática",
+    "salario": 5000
+  },
+  {
+    "id": 2,
+    "nome": "Carlos Oliveira",
+    "idade": 45,
+    "data_nascimento": "1978-11-22",
+    "disciplina": "Física",
+    "salario": 6000
+  },
+  {
+    "id": 3,
+    "nome": "Fernanda Costa",
+    "idade": 28,
+    "data_nascimento": "1995-03-10",
+    "disciplina": "Química",
+    "salario": 4800
+  },
+  {
+    "id": 4,
+    "nome": "João Pereira",
+    "idade": 50,
+    "data_nascimento": "1973-07-30",
+    "disciplina": "Biologia",
+    "salario": 7000
+  },
+  {
+    "id": 5,
+    "nome": "Mariana Santos",
+    "idade": 35,
+    "data_nascimento": "1988-09-05",
+    "disciplina": "História",
+    "salario": 4500
+  },
+  {
+    "id": 6,
+    "nome": "Ricardo Almeida",
+    "idade": 40,
+    "data_nascimento": "1983-01-20",
+    "disciplina": "Geografia",
+    "salario": 5500
+  },
+  {
+    "id": 7,
+    "nome": "Tatiane Lima",
+    "idade": 32,
+    "data_nascimento": "1991-12-12",
+    "disciplina": "Educação Física",
+    "salario": 5200
+  }
+]
+
+Turmas = [
+  {
+    "id": 1,
+    "nome": "Turma A",
+    "turno": "Manhã",
+    "professor_id": 1
+  },
+  {
+    "id": 2,
+    "nome": "Turma B",
+    "turno": "Tarde",
+    "professor_id": 2
+  },
+  {
+    "id": 3,
+    "nome": "Turma C",
+    "turno": "Noite",
+    "professor_id": 3
+  },
+  {
+    "id": 4,
+    "nome": "Turma D",
+    "turno": "Manhã",
+    "professor_id": 4
+  },
+  {
+    "id": 5,
+    "nome": "Turma E",
+    "turno": "Tarde",
+    "professor_id": 5
+  },
+  {
+    "id": 6,
+    "nome": "Turma F",
+    "turno": "Noite",
+    "professor_id": 6
+  },
+  {
+    "id": 7,
+    "nome": "Turma G",
+    "turno": "Manhã",
+    "professor_id": 7
+  }
+]
+    
+
